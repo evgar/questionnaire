e5a4176a7ca5bdbc9e1cce018663f20ea61ad795
evgar <eugene.garichev@gmail.com>
2016-11-25 14:46:45 +0200

    fix subject-info js

--- app/blocks/subject-info/subject-info.js
+++ app/blocks/subject-info/subject-info.js
@@ -1,20 +1,14 @@
-// window.document.ready(function () {
-	global.$(window.document)
-		.one('focus.autoExpand', 'textarea.autoExpand', function(){
-			// debugger;
-			var savedValue = this.value;
-			this.value = '';
-			this.baseScrollHeight = this.scrollHeight;
-			console.log('baseScrollHeight:' + this.baseScrollHeight);
-			this.value = savedValue;
-		})
-		.on('input.autoExpand', 'textarea.autoExpand', function(){
-			var minRows = this.getAttribute('data-min-rows')|0, rows;
-			this.rows = minRows;
-			console.log('Rows:' + this.rows);
-			console.log('scrollHeight:' + this.scrollHeight);
-			rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 60);
-			console.log(rows);
-			this.rows = minRows + rows;
-		});
-// });
+global.$(window.document)
+	.one('focus', 'textarea#infoText', function(){
+		const savedValue = this.value;
+		this.value = '';
+		this.baseScrollHeight = this.scrollHeight;
+		this.value = savedValue;
+	})
+	.on('input', 'textarea#infoText', function(){
+		const minRows = this.getAttribute('data-min-rows')|0;
+		let rows;
+		this.rows = minRows;
+		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 60);
+		this.rows = minRows + rows;
+	});
