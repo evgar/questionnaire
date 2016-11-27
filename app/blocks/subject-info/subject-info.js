2bdf7fa0a6016fb3b489d84d401297f732f0716e
evgar <eugene.garichev@gmail.com>
2016-11-27 12:56:03 +0200

    fix selector names & update subject-info js

--- app/blocks/subject-info/subject-info.js
+++ app/blocks/subject-info/subject-info.js
@@ -1,14 +1,18 @@
-global.$(window.document)
-	.one('focus', 'textarea#infoText', function(){
-		const savedValue = this.value;
-		this.value = '';
-		this.baseScrollHeight = this.scrollHeight;
-		this.value = savedValue;
+$(window.document)
+	.on('ready', function () {
+		$('#infoText').val('В CSSSR меня привлекли интересные проекты, возможность изучить новые технологии и опробовать их на практике, то, что у вас отлажена система работы с удаленными сотрудниками, ' +
+			'имеются исчерпывающие инструкции и сборщик проектов.');
+		$('#infoText').attr('rows', '7');
 	})
-	.on('input', 'textarea#infoText', function(){
+	.on('focus', '#infoText', function(){
+		$('#infoText').val('');
+		$('#infoText').attr('rows', '3');
+		this.baseScrollHeight = this.scrollHeight;
+	})
+	.on('input', '#infoText', function(){
 		const minRows = this.getAttribute('data-min-rows')|0;
 		let rows;
 		this.rows = minRows;
-		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 60);
+		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight)/60);
 		this.rows = minRows + rows;
 	});
