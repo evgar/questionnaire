3f84d977e879a96457f6ccf95679d0b7656ffe44
evgar <eugene.garichev@gmail.com>
2017-02-10 10:32:47 +0200

    fix elements nesting in subject-info block

--- app/blocks/textfield/subject-info.js
+++ app/blocks/textfield/subject-info.js
@@ -1,15 +1,17 @@
+var textArea = $('.textfield');
+
 $(window.document)
 	.on('ready', function () {
-		$('#infoText').val('В CSSSR меня привлекли интересные проекты, возможность изучить новые технологии и опробовать их на практике, то, что у вас отлажена система работы с удаленными сотрудниками, ' +
+		textArea.val('В CSSSR меня привлекли интересные проекты, возможность изучить новые технологии и опробовать их на практике, то, что у вас отлажена система работы с удаленными сотрудниками, ' +
 			'имеются исчерпывающие инструкции и сборщик проектов.');
-		$('#infoText').attr('rows', '7');
+		textArea.attr('rows', '7');
 	})
-	.on('focus', '#infoText', function(){
-		$('#infoText').val('');
-		$('#infoText').attr('rows', '3');
+	.on('focus', '.textfield', function(){
+		textArea.val('');
+		textArea.attr('rows', '3');
 		this.baseScrollHeight = this.scrollHeight;
 	})
-	.on('input', '#infoText', function(){
+	.on('input', '.textfield', function(){
 		const minRows = this.getAttribute('data-min-rows')|0;
 		let rows;
 		this.rows = minRows;
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import '../blocks/js-level/js-level.js';
-import '../blocks/subject-info/subject-info.js';
+import '../blocks/textfield/subject-info.js';
 
 $(() => {
 	svg4everybody();
